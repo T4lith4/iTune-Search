fbe1416fa8785df178e44f4e446489a4d0a3a385
Talitha <talkruger9@gmail.com>
2020-01-30 11:27:10 +0200

    -added build

---
+++ build/precache-manifest.9ca6b53fe5d8a7d7804a10309128799a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deca97c51290626eae0645ea96bca265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff9c641c5e1cf879eb0",
+    "url": "/static/css/main.c759bee7.chunk.css"
+  },
+  {
+    "revision": "9bffe0f0e08cc3dbe819",
+    "url": "/static/js/2.4e5938c0.chunk.js"
+  },
+  {
+    "revision": "d18c27b3237c69ab76bf03d5dddf8e43",
+    "url": "/static/js/2.4e5938c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aff9c641c5e1cf879eb0",
+    "url": "/static/js/main.caecfb1d.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "cc195f1b4b6e675b32ee20b9e0c1ddfa",
+    "url": "/static/media/audiobook.cc195f1b.png"
+  },
+  {
+    "revision": "488230c28124a50f5d36a693bca35454",
+    "url": "/static/media/home.488230c2.jpg"
+  },
+  {
+    "revision": "de50e2e9ca0fc52b57dbe9600b542e19",
+    "url": "/static/media/loader.de50e2e9.gif"
+  },
+  {
+    "revision": "e1468bd4238b97345677cc4a8fc954d4",
+    "url": "/static/media/video-itunes.e1468bd4.png"
+  }
+]);
--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use(function(err, req, res, next) {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
@@ -73,3 +73,6 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+
+
